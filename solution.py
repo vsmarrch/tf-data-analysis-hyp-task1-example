0407ac49d041fb03d22bb6d9cf1b0cbc1879e60d
Viktoria Marchenko <92745008+vsmarrch@users.noreply.github.com>
2023-04-09 03:53:04 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,13 +2,20 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 260376781 # Ваш chat ID, не меняйте название переменной
+
+from scipy.stats import norm
 
 def solution(x_success: int, 
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    alpha = 0.02
+    p_y = y_success / y_cnt
+    p_x = x_success / x_cnt
+    p = (y_success + x_success) / (y_cnt + x_cnt)
+    std = np.sqrt(p * (1 - p) / y_cnt + p * (1 - p) / x_cnt)
+    z = (p_y - p_x) / std
+    z_crit = norm.ppf(1 - alpha)
+    answer = True if z > z_crit else False
+    return answer
